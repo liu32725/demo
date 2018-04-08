65ad3d2429d0a4eea4336dbc3a34aea1f41e4a79
liu <liu32725@163.com>
2018-04-08 17:39:42 +0800

    111

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+/**
+ * Created by Administrator on 2018/4/8 0008.
+ */
+public class Test {
+    private static Test ourInstance = new Test();
+
+    public static Test getInstance() {
+        return ourInstance;
+    }
+
+    private Test() {
+    }
+}
